0f6c1429d89246bc726b302e286afa26f1ae924f
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2021-05-27 15:34:47 +0530

    adds pendingIntent

--- app/src/main/java/com/example/eggtimer/utils/NoitificationUtils.kt
+++ app/src/main/java/com/example/eggtimer/utils/NoitificationUtils.kt
@@ -1,13 +1,24 @@
 package com.example.eggtimer.utils
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import androidx.core.app.NotificationCompat
+import com.example.eggtimer.MainActivity
 import com.example.eggtimer.R
 
 val NOTIFICATION_ID = 0
 
 fun NotificationManager.sendNotification(messageBody:String, applicationContext : Context){
+    
+    val contentIntent = Intent(applicationContext,MainActivity::class.java)
+    val contentPendingIntent = PendingIntent.getActivity(
+        applicationContext,
+        NOTIFICATION_ID,
+        contentIntent,
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
 
     val builder = NotificationCompat.Builder(
         applicationContext,
@@ -17,6 +28,8 @@ fun NotificationManager.sendNotification(messageBody:String, applicationContext 
         .setContentTitle(applicationContext.getString(R.string.notificationTitle))
         .setContentText(messageBody)
         .setPriority(NotificationManager.IMPORTANCE_HIGH)
+        .setContentIntent(contentPendingIntent)
+        .setAutoCancel(true)
 
     notify(NOTIFICATION_ID,builder.build())
 }
